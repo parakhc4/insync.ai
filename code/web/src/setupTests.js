2ccded8a8b352523eff2eff6d9ef3947a50ea4d0
Parakh Chaudhary <69101765+parakhc4@users.noreply.github.com>
2025-06-08 21:14:54 +0530

    Add activity planner feature and driver view

--- code/web/src/setupTests.js
+++ code/web/src/setupTests.js
@@ -3,3 +3,19 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Polyfill window.matchMedia for components relying on it (e.g., react-slick)
+if (!window.matchMedia) {
+  window.matchMedia = function(query) {
+    return {
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: () => {},
+      removeListener: () => {},
+      addEventListener: () => {},
+      removeEventListener: () => {},
+      dispatchEvent: () => false
+    };
+  };
+}
